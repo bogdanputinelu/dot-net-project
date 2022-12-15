84397a5585331abf7e42096426c952542b639d9c
bogdanputinelu <bogdanputinelu@yahoo.com>
2022-12-15 13:31:52 +0200

    added context and connection to database

---
+++ Project/Project/Data/ProjectContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models;
+
+namespace Project.Data
+{
+    public class ProjectContext: DbContext 
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ProjectContext(DbContextOptions<ProjectContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
